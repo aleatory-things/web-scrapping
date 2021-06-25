4a504f580012e9bf54595e34d11d8d758535f958
Katacoda Scenario <scenario@katacoda.com>
2021-06-25 17:38:21 +0000

    modify just a bit

--- app.js
+++ app.js
@@ -2,11 +2,14 @@ const rp = require('request-promise');
 const cheerio = require('cheerio')
 const url = 'https://en.wikipedia.org/wiki/List_of_Presidents_of_the_United_States';
 
+
 rp(url)
   .then(function(html){
-    const $ = cheerio.load(html)
-    
-    const tabRow = $('tbody', html).find('tr').find('th').text().split(' ')
+    let presidentsQuantity = []
+    const $ = cheerio.load(html)    
+    let tabRow = $('tbody > tr > th', html).text().split(' ')  
+    tabRow = tabRow[4].split('\n')
+    console.log(tabRow)      
     
   })
   .catch(function(err){
